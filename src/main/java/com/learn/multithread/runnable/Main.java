dfa6a13567eb77f8eafc2d74ae0a9f3164118adf
yossi78 <yossi78@gmail.com>
2023-04-19 14:28:23 +0300

    Add new code

--- src/main/java/com/learn/multithread/runnable/Main.java
+++ src/main/java/com/learn/multithread/runnable/Main.java
@@ -6,7 +6,6 @@ public class Main{
 
     public static void main(String[] args) throws InterruptedException{
 
-
         // Create a subclass of Thread
         MyThread thread1 = new MyThread();
         //or
